e5eb0f4af544bf0a87ef7844a80e83d2a21ae04b
E200851 <j.komwabo@student.helmo.be>
2022-08-07 23:17:31 +0200

    Projet Aout 2022

--- JITC/Program.cs
+++ JITC/Program.cs
@@ -2,18 +2,25 @@ using JITC.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
+
+
+var builder = WebApplication.CreateBuilder(args);
+var builderConfig = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("default");
+builder.Services.AddDbContext<JITCDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<JITCDbContext>(
- options => options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; 
-                 Database=JITCDb; Trusted_Connection=True"));
-//options => options.UseSqlServer(@"server=192.168.128.18;Database=in20b1125,user-id=in20b1125;password=8903"));
-/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<JITCDbContext>();*/
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireDigit = false; options.Password.RequiredLength = 4; options.Password.RequireUppercase = false;
@@ -23,8 +30,12 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+}
+else 
+{ 
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
---
+++ JITC/gulpTask.js
@@ -0,0 +1,19 @@
+﻿'use strict'
+
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    del = require('del')
+
+
+var webrootFolder = "wwwroot";
+var paths = { webroot: "./" + webrootFolder + "/" };
+paths.js = paths.webroot + "js/**/*.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.mylib = paths.webroot + "mylib";
+
+gulp.task("clean", function () { return del.sync(paths.mylib + "/*") });
+gulp.task("minjs", function () { return gulp.src(paths.js).pipe(concat("all.min.js")).pipe(uglify()).pipe(gulp.dest(paths.mylib)) });
+gulp.task("mincss", function () { return gulp.src(paths.css).pipe(concat("style.js")).pipe(cssmin()).pipe(gulp.dest(paths.mylib)) });
